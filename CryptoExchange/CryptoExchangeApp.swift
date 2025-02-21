ba301be770050aeb619bafaeae2fb927addf1001
xGentuso <ryan.mota@triosstudent.com>
2025-02-20 22:20:58 -0500

    Initial Commit

---
+++ CryptoExchange/CryptoExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoExchangeApp.swift
+//  CryptoExchange
+//
+//  Created by ryan mota on 2025-02-20.
+//
+
+import SwiftUI
+
+@main
+struct CryptoExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
